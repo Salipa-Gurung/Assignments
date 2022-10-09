cfe3ae873967a41c34822bc833f3be7e9d4a5be3
Salipa-Gurung <salipagurung@gmail.com>
2022-10-09 22:22:03 +0545

    created random seat with undefined error

--- JankariTech-mentorship/randomSeatPlanning.js
+++ JankariTech-mentorship/randomSeatPlanning.js
@@ -2,33 +2,32 @@ import PromptSync from "prompt-sync";
 
 const prompt = PromptSync();
 
-function randomNo(){
+function randomSeatGenerator(){
     const total = prompt('Enter total number of students: ');
     const studentName = new Array(total);
     const result = new Array(total);
     for(let i = 0; i < total; i++) {
         studentName [i] = prompt('Enter Name: ');
     }
-    // console.log("studentName array ko");
-    // for(let i=0;i<total;i++){
-    //     console.log(studentName[i]);
-    // }
-    // console.log("------------------");
-    
-    // do{
-        for(let i = 0; i < total; i++){
-            const rndmNo = Math.floor(Math.random() * total);
-            if(result[rndmNo] == null){
-                result[rndmNo] = studentName[i];
-                console.log(result[rndmNo]);
-            }
+    for(let i = 0; i < total; i++) {
+        const randomNo = Math.floor(Math.random() * total);
+        const temp = studentName[randomNo];
+        if(!(result.includes(temp))) {
+            result[i] = studentName[randomNo];
         }
-    // }while(result.every(r => studentName.includes(r)))
+    }
+    
+    console.log("---------------Shuffled Seat--------------");
+    for(let i = 0; i < total; i++){
+        console.log(result[i]);
+    }
+    console.log("------------------------------------------");
+        // for(let i = 0; i < total; i++){
+        //     const rndmNo = Math.floor(Math.random() * total);
+        //     if(result[rndmNo] == null){
+        //         result[rndmNo] = studentName[i];
+        //         console.log(result[rndmNo]);
+        //     }
+        // }
 }
-randomNo();
-
-// const total = prompt('Enter total number of students: ');
-// var studentName = new Array(total);
-// for(let i = 0; i < total; i++) {
-//     studentName [i] = prompt('Enter Name: ');
-// }
+randomSeatGenerator();

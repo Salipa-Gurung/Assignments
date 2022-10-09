604e0196bc037ac4906d97324096a67f663a3a9a
Salipa-Gurung <salipagurung@gmail.com>
2022-10-09 23:14:29 +0545

    added comment messages

--- JankariTech-mentorship/randomSeatPlanning.js
+++ JankariTech-mentorship/randomSeatPlanning.js
@@ -1,33 +1,36 @@
+// --------------------Random Seat Planning-----------------------
 import PromptSync from "prompt-sync";
 
 const prompt = PromptSync();
 
 function randomSeatGenerator(){
-    const total = prompt('Enter total number of students: ');
+
+    const total = prompt('Enter total number of students: '); //for array size of seat and studentName
+
+    // creating array of size -> total
     const studentName = new Array(total);
-    const result = new Array(total);
+    const seatNo = new Array(total);
+
+    // loop for asking names
     for(let i = 0; i < total; i++) {
         studentName [i] = prompt('Enter Name: ');
     }
+
     for(let i = 0; i < total; i++) {
-        const randomNo = Math.floor(Math.random() * total);
+        const randomNo = Math.floor(Math.random() * total);  //Generating random number (from 0 to total-1)
         const temp = studentName[randomNo];
-        if(!(result.includes(temp))) {
-            result[i] = studentName[randomNo];
+
+        // checking if person is already assigned to seats
+        if(!(seatNo.includes(temp))) {
+            seatNo[i] = temp; //assigning a person to a seatNo of index i
         }
     }
-    
+    // loop to output all random seat  
     console.log("---------------Shuffled Seat--------------");
     for(let i = 0; i < total; i++){
-        console.log(result[i]);
+        console.log("Seat " + (i+1) + ": " + seatNo[i]);
     }
     console.log("------------------------------------------");
-        // for(let i = 0; i < total; i++){
-        //     const rndmNo = Math.floor(Math.random() * total);
-        //     if(result[rndmNo] == null){
-        //         result[rndmNo] = studentName[i];
-        //         console.log(result[rndmNo]);
-        //     }
-        // }
 }
+
 randomSeatGenerator();
